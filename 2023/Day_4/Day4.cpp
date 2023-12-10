0b72fced7674290911b2b627a89ef9e3c0dfeb46
ivarhman <ivar_mannsaaker@hotmail.com>
2023-12-10 15:07:35 +0100

    day4
    finished day 4 part 2

--- 2023/Day_4/Day4.cpp
+++ 2023/Day_4/Day4.cpp
@@ -50,7 +50,7 @@ int part1() {
         std::cerr << "File not found \n";
     }
     std::cout << output << "\n";
-
+    return output;
 
 }
 
@@ -61,21 +61,30 @@ int part2() {
     std::string ownNumbers;
 
     int output = 0;
+    int array[189];
+    int matches = 0;
+    for (int i = 0; i < 189; i++) {
+        array[i] = 1;
+    }
 
     if (file.is_open()) {
         int row = 0;
         for (std::string line; std::getline(file, line);) {
             winningNumbers = line.substr(10, 29);
             ownNumbers = line.substr(42, 100);
-            
+            matches = 0;
+
             std::vector<int> winningNumbersList = extractNumbers(winningNumbers);
             std::vector<int> ownNumbersList = extractNumbers(ownNumbers);
             for (int number : ownNumbersList) {
                 if (std::find(winningNumbersList.begin(), winningNumbersList.end(), number) != winningNumbersList.end()) {
-                    output += 1 * array[];
-                    std::cout << "Found: " << number << "Sum: " << sum << "\n";
+                    matches += 1;
                 }
             }
+            for (int i = 0; i< matches; i++) {
+                array[row+i+1] += array[row];
+            }
+            output += array[row];
         row += 1;
         }
 
@@ -84,11 +93,12 @@ int part2() {
         std::cerr << "File not found \n";
     }
     std::cout << output << "\n";
-
+    return output;
 };
 
 
-void main() {
+int main() {
     // part1();
     part2();
+    return 1;
 };
---
+++ 2023/Day_5/Day5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int part1() {
+
+    int output = 0;
+
+    std::fstream file("input.txt");
+
+
+    return output;
+}
+
+
+int main() {
+    part1();
+}

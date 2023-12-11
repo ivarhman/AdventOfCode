8f613752b0efa706a7208617be0e4a54ca53ae13
ivarhman <ivar_mannsaaker@hotmail.com>
2023-12-11 09:34:46 +0100

    added .txt to gitgignore
    added solution for day 5 part 1 and **.txt to .gitignore

--- 2023/Day_5/Day5.cpp
+++ 2023/Day_5/Day5.cpp
@@ -2,55 +2,85 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
-std::vector<int> mapToNewPoint(std::vector<std::vector<int>> map, std::vector<int> seeds) {
-    int output = 0;
+std::vector<long long> mapToNewPoint(std::vector<std::vector<long long>> map, std::vector<long long> seeds) {
+    long long output = 0;
+    std::vector<long long> newSeeds;
+    bool seeded = false;
 
-    std::vector<int> newSeeds;
-
-    for (int seed: seeds) {
+    for (long long seed: seeds) {
         for (const auto& row : map) {
-            int destinaton = row[0];
-            int source = row[1];
-            int range = row[2];
+            long long destinaton = row[0];
+            long long source = row[1];
+            long long range = row[2];
 
             if (seed >= source && seed < source + range) {
                 output = seed - source +  destinaton;
                 newSeeds.push_back(output);
+                seeded = true;
+                break;
             };
         };
-        newSeeds.push_back(seed);
+        if (!seeded) {
+            newSeeds.push_back(seed);
+        }
+        seeded = false;
     };
     return newSeeds;
 }
 
-std::vector<int> extractNumbers(const std::string& str) {
+std::vector<long long> extractNumbers(const std::string& str) {
     std::stringstream ss(str);
-    int num;
-    std::vector<int> numbers;
+    long long num;
+    std::vector<long long> numbers;
     while (ss >> num) {
         numbers.push_back(num);
     };
     return numbers;
 }
 
-int part1() {
+long long part1() {
 
-    int output = 0;
+    long long output = 0;
+    bool seedFound = false;
+    std::vector<long long> seeds;
+    std::vector<long long> originalSeeds;
+    std::vector<std::vector<long long>> map;
 
     std::fstream file("input.txt");
 
-    std::vector<int> seeds;
 
     if (file.is_open()) {
         
         for (std::string line; std::getline(file, line);) {
-            if (line[5]== ":") {
-                seeds = extractNumbers(line);
-            }
             if (line == "") {
-                std::cout << "Empty line" << "\n";
+                if (seedFound) {
+                    std::cout << "Seeds:\t" << seeds.size() << "\n";
+                    seeds = mapToNewPoint(map, seeds);
+                }
+                map = {};
+                continue;
             }
+
+            if (isdigit(line[0])) {
+                std::vector <long long> mapline = extractNumbers(line);
+                map.push_back(mapline);
+                continue;
+            }
+
+            if (line.find("seeds") != std::string::npos) {
+                line = line.substr(7, 212);
+                std::cout << "Seeds :" << line << "\n";
+                seeds = extractNumbers(line);
+                originalSeeds = seeds;
+                seedFound = true; 
+            }
+
+        }
+        if (seedFound) {
+            std::cout << "Seeds:\t" << seeds.size() << "\n";
+            seeds = mapToNewPoint(map, seeds);
         }
 
         file.close();
@@ -58,11 +88,78 @@ int part1() {
         std::cout << "File not found" << "\n";
     }
 
+    int i = 1;
+    for (long long number: seeds) {
+        std::cout << number << "\t" << i << "\n";
+        i++;
+    }
+    output = std::min_element(seeds.begin(), seeds.end())[0];
+    return output;
+}
 
+long long part2() {
+
+    long long output = 0;
+    bool seedFound = false;
+    std::vector<long long> seeds;
+    std::vector<long long> originalSeeds;
+    std::vector<std::vector<long long>> map;
+
+    std::fstream file("input.txt");
+
+
+    if (file.is_open()) {
+        
+        for (std::string line; std::getline(file, line);) {
+            if (line == "") {
+                if (seedFound) {
+                    seeds = mapToNewPoint(map, seeds);
+                }
+                map = {};
+                continue;
+            }
+
+            if (isdigit(line[0])) {
+                std::vector <long long> mapline = extractNumbers(line);
+                map.push_back(mapline);
+                continue;
+            }
+
+            if (line.find("seeds") != std::string::npos) {
+                line = line.substr(7, 212);
+                seeds = extractNumbers(line);
+                for (int i=0; i<seeds.size(); i+=2) {
+                    long long start = seeds[i];
+                    long long endvalue = seeds[i+1] + seeds[i];
+                    std::cout << "Start: \t" << start << "\tEnd: \t" << endvalue << "\n";
+                    for (long long l = start; l < endvalue;l++) {
+                        originalSeeds.push_back(l);
+                    }
+                }
+                seeds = originalSeeds;
+                //originalSeeds = seeds;
+                seedFound = true; 
+            }
+
+        }
+        if (seedFound) {
+            seeds = mapToNewPoint(map, seeds);
+        }
+
+        file.close();
+    } else {
+        std::cout << "File not found" << "\n";
+    }
+
+    output = std::min_element(seeds.begin(), seeds.end())[0];
     return output;
 }
 
 
 int main() {
-    part1();
+    long long part1Solution = part1();
+    std::cout << "Part 1: " << part1Solution << "\n";
+
+    long long part2Solution = part2();
+    std::cout << "Part 2: " << part2Solution << "\n";
 }

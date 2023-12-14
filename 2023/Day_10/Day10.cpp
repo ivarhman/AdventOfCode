a38e269b31d1a99c4725d6eed5105f1c36ebd16e
ivarhman <ivar_mannsaaker@hotmail.com>
2023-12-14 09:25:35 +0100

    Update Day10.cpp
    finished part 1

--- 2023/Day_10/Day10.cpp
+++ 2023/Day_10/Day10.cpp
@@ -4,11 +4,14 @@
 #include <algorithm>
 #include <string>
 #include <cmath>
+#include <set>
+#include <unordered_map>
 
 
 class snakeMap {
     private: std::vector<std::string> map;
     private: std::vector<int> position;
+    private: std::vector<std::vector<int>> trail;
 
 
     public: snakeMap(std::string inputfile) {
@@ -38,48 +41,92 @@ class snakeMap {
 
     };
 
+    private: std::vector<int> findStartdirection() {
+        std::vector<int> direction;
+        std::vector<std::vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        std::unordered_map<char, std::vector<std::vector<int>>> validDirections= {
+            {'F', {{-1,0}, {0,-1}}},
+            {'J', {{1,0}, {0,-1}}},
+            {'7', {{-1,0}, {0,-1}}},
+            {'L', {{1,0}, {0,1}}},
+            {'|', {{-1,0}, {1,0}}},
+            {'-', {{0,-1}, {0,1}}}
+        };
+        std::vector<int> position = this->position;
+        std::set<char> validPositions = {'F', 'J', '7', 'L','|', '-'};
+
+        for (int i = 0; i < 4; i++){
+            position = {this-> position[0] + directions[i][0], this->position[1] + directions[i][1]};
+            auto findChar = validPositions.find(this->map[position[0]][position[1]]);
+            if (findChar == validPositions.end()) {
+                continue;
+            }
+            char validChar = *findChar;
+            auto vectors = validDirections[validChar];
+            if (vectors[0] == directions[i] || vectors[1] == directions[i]) {
+                return directions[i];
+            }
+
+        }
+        return direction;
+    }
+
 
     public: int solveMappart1 () {
-        float steps = 0;
+        float steps = 1;
         std::vector<int> direction;
         std::vector<int> position = this->position;
 
 
         std::vector<std::vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        direction = directions[0];
-
-        while (true) {}
-
+        direction = findStartdirection();
+        position[0] += direction[0];
+        position[1] += direction[1];
+        this->trail.push_back(position);
         while (true) {
         switch (this->map[position[0]][position[1]]) {
             case 'F':
-                if (direction[0] == 1) {
+                if (direction[0] == -1) {
+                    direction = directions[0];
+                } else {
+                    direction = directions[1];
+                }
+                break;
+            case 'J':
+                if (direction[0] == 0) {
+                    direction = directions[3];
+                } else {
+                    direction = directions[2];
+                }
+                break;
+            case '7':
+                if (direction[0] == -1) {
+                    direction = directions[2];
+                } else {
+                    direction = directions[1];
+                }
+                break;
+            case 'L':
+                if(direction[0] == 1) {
                     direction = directions[0];
                 } else {
                     direction = directions[3];
                 }
                 break;
-            case 'J':
-                if (direction[0] == 0) {
-                    direction = directions[1];
-                } else {
-                    direction = directions[2];
-                }
-                break;
-            case '|':
-                break;
-            case '-':
-                break;
+                case '|': break;
+                case '-': break;
             default:
                 std::cerr << "Error: Invalid character in map\n";
                 break;}
             position[0] += direction[0];
             position[1] += direction[1];
+            this->trail.push_back(position);
+            steps ++;
 
             if (position == this->position) {
-                return std::ceil(steps);
-            }
-    }
+                return std::ceil((steps-1)/2);
+            };
+    };
     };
 };
 
@@ -87,10 +134,11 @@ class snakeMap {
 
 int main() {
 
-    std::string inputfile = "example.txt";
+    std::string inputfile = "input.txt";
     snakeMap playSnakes(inputfile);
 
+    std::cout << playSnakes.solveMappart1() << "\n";
 
 
     return 0;
-}
+};
